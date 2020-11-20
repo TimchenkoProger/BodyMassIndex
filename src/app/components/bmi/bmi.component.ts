b1f94f41da15e0aa1238fa7088f9a7d6799b6a15
TimchenkoArtem <artem.t200@gmail.com>
2020-11-20 22:40:53 +0200

    Did round for index and changed design!

--- src/app/components/bmi/bmi.component.ts
+++ src/app/components/bmi/bmi.component.ts
@@ -12,31 +12,31 @@ export class BmiComponent implements OnInit {
   backgroundsForTable:string[];
   constructor(){
     this.index=0;
-    this.backgroundsForTable=["","","",""];
+    this.backgroundsForTable=[];
   }
   ngOnInit(): void {
   }
   calculateBMI(height:any,mass:any)
   {
-    this.index=mass/((height/100)^2);
+    this.index=parseFloat((mass/((height/100)^2)).toFixed(2));
     if(this.index < 18.5)
     {
-      this.backgroundsForTable=["","","",""];
+      this.backgroundsForTable=[];
       this.backgroundsForTable[0] = "table-success";
     }
     else if(this.index < 24.9)
     {
-      this.backgroundsForTable=["","","",""];
+      this.backgroundsForTable=[];
       this.backgroundsForTable[1] = "table-success";
     }
     else if(this.index < 29.9)
     {
-      this.backgroundsForTable=["","","",""];
+      this.backgroundsForTable=[];
       this.backgroundsForTable[2] = "table-success";
     }
     else
     {
-      this.backgroundsForTable=["","","",""];
+      this.backgroundsForTable=[];
       this.backgroundsForTable[3] = "table-success";
     };
   }
